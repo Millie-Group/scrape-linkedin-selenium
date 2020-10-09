0db34de5fcc421a855e2135bfcfda37ee55d124a
Emily Y <emilygbs@yahoo.com>
2020-10-09 04:48:08 -0400

    scraping code with filtering for undergrad, yrs experience, and US/UK schools

--- scrape_linkedin/main.py
+++ scrape_linkedin/main.py
@@ -1,156 +1,152 @@
-from ProfileScraper import ProfileScraper
-import os
-from selenium.webdriver import Chrome
 import pandas as pd
-
-def scrape(url):
-    driver_options = {}
-    driver_type = Chrome
-
-    scraper = ProfileScraper(driver=driver_type, cookie='cookie', driver_options = driver_options)
-    profile = scraper.scrape(url=url)
-
-    output = profile.to_dict()
-
-    return output
+import sys
+from cookies import cookies
+from scrape_profile import scrape_profile
 
 
-def scrape_profile(url):
-    international = ["International Baccalaureate", "IBDP", "IB", "IB Score", "A Level", "French Baccalaureate"]
+# school lists to store the most common ones
+IVIES = ["Harvard University", "Yale University", "Princeton University", "Columbia University in the City of New York",
+         "University of Pennsylvania", "Brown University", "Dartmouth College", "Cornell University"]
+OXBRIDGE = ["University of Oxford", "University of Cambridge"]
 
-    data = scrape(url)
 
-    fullName = data['personal_info']['name']
-    firstName = fullName[:fullName.index(' ')]
-    lastName = fullName[fullName.rindex(' ')+1:]
+def school_list(schools, country):
+    if country == '':
+        return schools['School Name'].tolist()
+    else:
+        country_schools = []
+        for index, row in schools.iterrows():
+            if schools.loc[index, 'US/UK'] is country:
+                country_schools.append(schools.loc[index, 'School Name'])
+        return country_schools
 
-    #print("Full Name: ", fullName)
-    #print("First Name: ", firstName)
-    #print("Last Name: ", lastName)
-
-    headline = data['personal_info']['headline']
-
-    #print("Headline: ", headline)
-
-    location = data['personal_info']['location']
-
-    #print("Location: ", location)
-
-    university = []
-    undergrad = []
-    grad_yrs = []
-    undergrad_yr = 0
-    yrs_experience = 0
-    int_hs = 'N'
-
-    keepSchool = ''
-
-    for school in data['experiences']['education']:
-        schoolName = school['name']
-        degreeName = school['degree']
-
-        if 'College' in schoolName or 'University' in schoolName:
-            keepSchool = schoolName
-
-        if degreeName is not None and (degreeName[0] is 'B' or 'Bachelor' in degreeName):
-            undergrad.append(schoolName)
-            date_range = school['date_range']
-
-            if date_range is None:
-                undergrad_yr = 'N/A'
-            elif 'Present' not in date_range:
-                undergrad_yr = int(date_range[-4:])
-                grad_yrs.append(int(undergrad_yr))
-            elif date_range[0:4].isnumeric():
-                undergrad_yr = int(date_range[0:4]) + 4
-                grad_yrs.append(int(undergrad_yr))
-            else:
-                undergrad_yr = int(date_range[4:8])
-                grad_yrs.append(int(undergrad_yr))
-
-        else:
-            university.append(schoolName)
-
-            date_range = school['date_range']
-            yr = 0
-
-            if date_range is None:
-                yr = 'N/A'
-            elif 'Present' not in date_range:
-                yr = int(date_range[-4:])
-                grad_yrs.append(yr)
-            elif date_range[0:4].isnumeric():
-                yr = int(date_range[0:4]) + 4
-                grad_yrs.append(int(yr))
-            else:
-                yr = int(date_range[4:8])
-                grad_yrs.append(int(yr))
-
-        if degreeName is None:
-            degreeName = ''
-        if "High School" in schoolName or "High School" in degreeName or any(element in degreeName for element in international):
-            filtered = list(filter(lambda a: a is not None, school.values()))
-            if any(element in ' '.join(filtered) for element in international):
-                int_hs = 'Y'
-
-    if (undergrad_yr == 0 or undergrad_yr == 'N/A') and len(grad_yrs) > 0:
-        undergrad_yr = int(min(grad_yrs))
-
-    for experience in data['experiences']['jobs']:
-        date_range = experience['date_range']
-        date = 0
-
-        if date_range is None:
-            date = 'N/A'
-        elif date_range[4:8].isnumeric():
-            date = int(date_range[4:8])
-        elif date_range[0:4].isnumeric():
-            date = int(date_range[0:4])
-        else:
-            date = 'N/A'
-
-        if date is not 'N/A' and date < undergrad_yr:
-            undergrad_yr = date
-
-    yrs_experience = max(0, 2020 - int(undergrad_yr))
-    if yrs_experience == 2020:
-        yrs_experience = 0
-
-    if len(undergrad) == 0:
-        undergrad.append(keepSchool)
-
-    undergrad = ', '.join(undergrad)
-    #print("Undergrad: ", undergrad)
-
-    university = ', '.join(university)
-    #print("Other Schools: ", university)
-
-    email = data['personal_info']['email']
-    #print("Email: ", data['personal_info']['email'])
-
-    #print("International HS? ", int_hs)
-
-    return fullName, firstName, lastName, location, undergrad, university, yrs_experience, headline, int_hs, email
 
 def main():
-    excel_file = "BusinessP1.xlsx"
+    # choose LinkedIn account
+    name = input("Whose LinkedIn account should be used (Jenna, Guiseppe, or Bot)?\t")
+    name = name.lower()
+
+    if name in cookies:
+        cookie = cookies[name]
+    else:
+        sys.exit("Didn't enter a valid LinkedIn account! Must choose Jenna, Giuseppe, or Bot.")
+
+    # input name of spreadsheet (get profile links from this sheet and eventually return information to the same sheet)
+    filename = input("Name of spreadsheet?\t")
+
+    # choose undergrad options
+    undergrads = []
+    undergrad_enter = True
+    restrict_undergrad = input("Restrictions on undergrad schools (Y/N)?\t")
+    if restrict_undergrad == 'N': undergrad_enter = False
+    while undergrad_enter == True:
+        school = input("Input a school to include (1 for Ivies, 2 for Oxbridge, 3 for Ivies + Oxbridge, -1 to quit, or input your own):\t")
+        if school == "-1":
+            undergrad_enter = False
+        elif school == "1":
+            undergrads += IVIES
+        elif school == "2":
+            undergrads += OXBRIDGE
+        elif school == "3":
+            undergrads += IVIES + OXBRIDGE
+        else:
+            undergrads = undergrads.append(school)
+
+    # choose US or UK and read schools
+    toggle_country = int(input("If restricted undergrad schools are too limited, top US schools (1), top UK schools (2), either (3), or return all (4)?\t"))
+    undergrads_by_country = []
+
+    if toggle_country < 4:
+        schools_file = "../spreadsheets/Schools.xlsx"
+        schools = pd.read_excel(schools_file)
+        school_df = pd.DataFrame(schools)
+
+        undergrads_by_country = school_list(school_df, toggle_country)
+
+    # specify years of experience
+    exp_min = int(input("Preferred minimum years of experience after undergrad (-1 if no restriction)?\t"))
+    exp_max = int(input("Preferred maximum years of experience after undergrad (100 if no restriction)?\t"))
+
+    # minimum results
+    min_results = int(input("At least how many results?\t"))
+
+    # read links from spreadsheet
+    excel_file = "../spreadsheets/" + filename + ".xlsx"
     data = pd.read_excel(excel_file)
     df = pd.DataFrame(data)
 
-    for index, row in df.iterrows():
-        scraped_data = scrape_profile(row['LinkedIn URL'])
-        df.loc[index, 'Full Name'] = scraped_data[0]
-        df.loc[index, 'First Name'] = scraped_data[1]
-        df.loc[index, 'Last Name'] = scraped_data[2]
-        df.loc[index, 'Location'] = scraped_data[3]
-        df.loc[index, 'Undergrad'] = scraped_data[4]
-        df.loc[index, 'Other Schools'] = scraped_data[5]
-        df.loc[index, 'Yrs Exp'] = scraped_data[6]
-        df.loc[index, 'Headline'] = scraped_data[7]
-        df.loc[index, 'Int HS?'] = scraped_data[8]
-        df.loc[index, 'Email'] = scraped_data[9]
+    # set up variables to check for additional filters
+    exp_restrict_count = 0
+    exp_12_count = 0
 
-    df.to_excel('BusinessP1.xlsx')
+    undergrad_restrict_count = 0
+    undergrad_country_count = 0
+
+    # TODO: location
+
+    # scrape profiles
+    for index, row in df.iterrows():
+        url = row['LinkedIn URL']
+
+        if not pd.isnull(url) and pd.isnull(row['Full Name']):
+            if '?' in url:
+                url = url[:url.rindex('?')]
+            elif url[-1] is not '/':
+                url = url[:url.rindex('-')]
+            scraped_data = scrape_profile(url, cookie)
+
+            df.loc[index, 'LinkedIn URL'] = url
+            df.loc[index, 'Full Name'] = scraped_data[0]
+            df.loc[index, 'First Name'] = scraped_data[1]
+            df.loc[index, 'Last Name'] = scraped_data[2]
+            df.loc[index, 'Location'] = scraped_data[3]
+            df.loc[index, 'Undergrad'] = scraped_data[4]
+            df.loc[index, 'Other Schools'] = scraped_data[5]
+            df.loc[index, 'Yrs Exp'] = scraped_data[6]
+            df.loc[index, 'Headline'] = scraped_data[7]
+            df.loc[index, 'Int HS?'] = scraped_data[8]
+            df.loc[index, 'Email'] = scraped_data[9]
+            df.loc[index, 'Grad Yr'] = scraped_data[10]
+
+            # years of experience counts
+            if scraped_data[6] <= exp_max and scraped_data[6] >= exp_min: exp_restrict_count += 1
+            if scraped_data[6] <= 12: exp_12_count += 1
+
+            # undergrad counts
+            if scraped_data[4] in undergrads: undergrad_restrict_count += 1
+            if undergrads_by_country is not [] and scraped_data[4] in undergrads_by_country: undergrad_country_count += 1
+
+    # save to database -- commented out because no database yet
+    # TODO: figure out how this would interact with PostgreSQL database (instead of Excel file)
+    # reader = pd.read_excel(r'../spreadsheets/database.xlsx')
+    # df.to_excel('../spreadsheets/database.xlsx', index=False, header=False, startrow=len(reader) + 1)
+
+    # filter by years of experience
+    print("exp restrict count", exp_restrict_count)
+    print("exp 12 count", exp_12_count)
+    if exp_restrict_count >= min_results:
+        for index, row in df.iterrows():
+            if df.loc[index, 'Yrs Exp'] > exp_max or df.loc[index, 'Yrs Exp'] < exp_min:
+                df = df.drop(index)
+    elif exp_12_count >= min_results:
+        for index, row in df.iterrows():
+            if df.loc[index, 'Yrs Exp'] > 12:
+                df = df.drop(index)
+
+    # filter by undergrad
+    print("undergrad restrict count", undergrad_restrict_count)
+    print("undergrad country count", undergrad_country_count)
+    if undergrad_restrict_count >= min_results:
+        for index, row in df.iterrows():
+            if df.loc[index, 'Undergrad'] not in undergrads:
+                df = df.drop(index)
+    elif undergrads_by_country is not [] and undergrad_country_count >= min_results:
+        for index, row in df.iterrows():
+            if df.loc[index, 'Undergrad'] not in undergrads_by_country:
+                df = df.drop(index)
+
+    # save to initial spreadsheet
+    df.to_excel('../spreadsheets/' + filename + '.xlsx', index=False)
 
 
 if __name__ == "__main__":
---
+++ scrape_linkedin/scrape_profile.py
@@ -0,0 +1,141 @@
+from ProfileScraper import ProfileScraper
+from selenium.webdriver import Chrome
+
+
+# TODO: Look into using headless instead of Chrome
+def scrape(url, cookie):
+    driver_options = {}
+    driver_type = Chrome
+
+    scraper = ProfileScraper(driver=driver_type, cookie=cookie, driver_options = driver_options)
+    profile = scraper.scrape(url=url)
+
+    output = profile.to_dict()
+
+    return output
+
+
+# TODO: comment code and separate into methods
+def scrape_profile(url, cookie):
+    # TODO: remove once Ritvik sends school list
+    international = ["International Baccalaureate", "IBDP", "IB", "IB Score", "A Level",
+                     "A-Level", "French Baccalaureate", "AP", "Advanced Placement", "A Levels", "A*"]
+
+    data = scrape(url, cookie)
+
+    fullName = data['personal_info']['name']
+    firstName = fullName[:fullName.index(' ')]
+    lastName = fullName[fullName.rindex(' ')+1:]
+
+    #print("Full Name: ", fullName)
+    #print("First Name: ", firstName)
+    #print("Last Name: ", lastName)
+
+    headline = data['personal_info']['headline']
+
+    #print("Headline: ", headline)
+
+    location = data['personal_info']['location']
+
+    #print("Location: ", location)
+
+    university = []
+    undergrad = []
+    grad_yrs = []
+    undergrad_yr = 0
+    yrs_experience = 0
+    int_hs = 'N'
+
+    keepSchool = ''
+
+    for school in data['experiences']['education']:
+        schoolName = school['name']
+        degreeName = school['degree']
+
+        if 'College' in schoolName or 'University' in schoolName:
+            keepSchool = schoolName
+
+        if degreeName is not None and (degreeName[0] is 'B' or 'Bachelor' in degreeName):
+            undergrad.append(schoolName)
+            date_range = school['date_range']
+
+            if date_range is None:
+                undergrad_yr = 'N/A'
+            elif 'Present' not in date_range:
+                undergrad_yr = int(date_range[-4:])
+                grad_yrs.append(int(undergrad_yr))
+            elif date_range[0:4].isnumeric():
+                undergrad_yr = int(date_range[0:4]) + 4
+                grad_yrs.append(int(undergrad_yr))
+            else:
+                undergrad_yr = int(date_range[4:8])
+                grad_yrs.append(int(undergrad_yr))
+
+        else:
+            university.append(schoolName)
+
+            date_range = school['date_range']
+            yr = 0
+
+            if date_range is None:
+                yr = 'N/A'
+            elif 'Present' not in date_range:
+                yr = int(date_range[-4:])
+                grad_yrs.append(yr)
+            elif date_range[0:4].isnumeric():
+                yr = int(date_range[0:4]) + 4
+                grad_yrs.append(int(yr))
+            else:
+                yr = int(date_range[4:8])
+                grad_yrs.append(int(yr))
+
+        if degreeName is None:
+            degreeName = ''
+        if any(element.lower() in degreeName.lower() for element in international) or any(element.lower() in schoolName.lower() for element in international):
+            filtered = list(filter(lambda a: a is not None, school.values()))
+            if any(element in ' '.join(filtered) for element in international):
+                int_hs = 'Y'
+
+    if undergrad_yr == 'N/A':
+        undergrad_yr = 0
+
+    first_work = 2021
+
+    for experience in data['experiences']['jobs']:
+        date_range = experience['date_range']
+
+        if date_range is None:
+            date = 'N/A'
+        elif date_range[4:8].isnumeric():
+            date = int(date_range[4:8])
+        elif date_range[0:4].isnumeric():
+            date = int(date_range[0:4])
+        else:
+            date = 'N/A'
+
+        if date is not 'N/A':
+            if date < first_work:
+                first_work = date
+
+    if undergrad_yr == 0 and first_work < 2021:
+        yrs_experience = max(0, 2020 - int(first_work))
+    else:
+        yrs_experience = max(0, 2020 - int(undergrad_yr))
+    if yrs_experience == 2020:
+        yrs_experience = 0
+
+    if len(undergrad) == 0:
+        undergrad.append(keepSchool)
+
+    undergrad = ', '.join(undergrad)
+    #print("Undergrad: ", undergrad)
+
+    university = ', '.join(university)
+    #print("Other Schools: ", university)
+
+    email = data['personal_info']['email']
+    #print("Email: ", data['personal_info']['email'])
+
+    #print("International HS? ", int_hs)
+
+    return fullName, firstName, lastName, location, undergrad, university, yrs_experience, headline, int_hs, email, undergrad_yr
